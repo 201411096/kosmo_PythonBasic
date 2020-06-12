a38e695aaad80f3281b8c38999d46004e0782e50
김용선 <korea5781@gmail.com>
2020-06-12 15:13:29 +0900

    200612_05 linux install ( 리눅스 설치 )

--- aBasic/a_datatype_class/test.py
+++ aBasic/a_datatype_class/test.py
@@ -1,33 +1,9 @@
-#1
-# a = 777
-# b = 777
-# print(a == b, a is b)
-#3
-a=3.5
-b=int(3.5)
+#1 3
+#2 21
+#3 Gachon Human
+#4 1 -> 2
+#5 1010102
+#6 aa
+#7 81 -> 8,1
+#8 3
 
-print(a**((a//b) * 2))
-print(((a-b)*a)//b)
-b=(((a-b)*a)%b)
-print(b)
-print( (a * 4) % ( b * 4 ) )
-
-a=10.6
-b=10.5
-print(a*b)
-print(type(a+b))
-
-a="3.5"
-b=4
-print(a*b)
-a="3.5"
-b="1.5"
-print(a+b)
-
-a='3'
-b=float(a)
-print(b ** int(a))
-
-a=20
-b=4
-print(type((a / b)))

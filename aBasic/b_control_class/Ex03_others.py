720fbc07ead26e363fb7eaefc95be9078bad24f0
김용선 <korea5781@gmail.com>
2020-06-15 12:25:36 +0900

    200615_05

--- aBasic/b_control_class/Ex03_others.py
+++ aBasic/b_control_class/Ex03_others.py
@@ -1,6 +1,41 @@
-str = 'HELLO'                # 문자열
-li = ['a','b','c']        # 리스트
-tpl = ('ㄱ','ㄴ','ㄷ')    # 튜플
-di = dict(k=5, j=6)       # 딕셔너리
+msg = '밥먹자'                   # 문자열
+li = ['a','b','c']              # 리스트
+tpl = ('ㄱ','ㄴ','ㄷ')           # 튜플
+di = dict(k=5, j=6, i=7)        # 딕셔너리
+    # ㄴ di = {k-5, j=6, i=7} 과 동일한 코드
 
+#----------------------------------------
+# (1) unpacking : 각 요소를 분해
+c1, c2, c3 = di    # 문자열 뿐만 아니라 리스트, 튜플, 딕셔너리 전부 가능함
+print(c1, c2, c3)
 
+#----------------------------------------
+# (2) 리스트의 요소가 튜플인 경우
+alist = [ (1,2), (3,4), (5,6) ]
+for a in alist:
+    print(a)
+for (a, b) in alist:
+    print(a, b)
+    
+#----------------------------------------
+# (3) enumerate() 함수
+user_list = ['개발자', '코더', '전문가', '분석가']
+for user in user_list:
+    print(user)
+
+# index가 붙으면서 tuple로 받아옴
+for user in enumerate(user_list):
+    print(user)
+
+#----------------------------------------
+# (4) zip() : 여러 시퀀스를 순회하는 함수
+days = ['월', '화', '수']
+doit = ['잠자기', '놀기', '밥먹기', '공부']
+
+print(zip(days, doit))
+print(list(zip(days, doit))) # 짝꿍을 지어서 묶어줌
+print(dict(zip(days, doit))) # 짝꿍을 지어서 묶어줌
+
+mon=[6,7,8]
+print(list(zip(days, doit, mon)))
+# print(dict(zip(days, doit, mon))) # dictionary는 3개씩 묶을 수는 없음
--- aBasic/b_control_class/Ex04_comprehension.py
+++ aBasic/b_control_class/Ex04_comprehension.py
@@ -13,6 +13,7 @@
     * 셋 컨프리핸션
         { 표현식 for 표현식 in 순회가능객체 }
 
+    ***** tuple comprehension 은 존재하지 않음
 """
 
 
@@ -27,25 +28,43 @@ alist.append(6)
 print(alist)
 
 alist = []
-for n in range(1,6):
+for n in range(1, 7):
     alist.append(n)
 print(alist)
 
-alist = list(range(1,6))
+alist = list(range(1, 7))
 print(alist)
 
 
 #------------------------------------------------
 # 리스트 컨프리핸션
+blist = [n for n in range(1, 7)] # 1부터 6까지 뽑은 값으로 blist를 구성함
+print("blist1 =", blist)
+blist = [n**2 for n in range(1, 7) if n%2==1] #조건문을 먼저 체크하고 자승을 하는듯?
+print("blist2 =", blist)
 
-
+rows = range(1, 4) # 1,2,3
+cols = range(1, 3) # 1,2
+dlist = [(r,c) for r in rows for c in cols]
+print( "dlist = ", dlist)
 #-------------------------------------------
 # 딕셔러니 컨프리핸션
+data = (2,3,4)
+a = {x: x**2 for x in data}
+print(a)
 
-
-
+word = 'LOVE LOL'
+wcnt = {letter: word.count(letter) for letter in word}
+print(wcnt)
 #------------------------------------------------
 # 셋 컨프리핸션
+
+# 1부터 6까지 수 중에서 짝수만 셋에 담기
+# data03 = (1,2,3,4,5,6)
+#data03 = range(1, 7)
+data03 = [1,2,3,4,5,4,2,6] # 마지막 결과형태인 set을 따라서 2,4,6이 한번씩만 출력이 됨
+set01 = {x for x in data03 if x % 2 == 0}
+print(set01)
 
 
 
@@ -53,3 +72,9 @@ print(alist)
 # -------------------------------------------------
 # [참고] 제너레이터 컨프리핸션
 # ( ) 를 사용하면 튜플이라 생각하지만 튜플은 컨프리핸션이 없다.
+# 제너레이터 : 요소를 추출하면서 처리하고 저장을 하지는 않음
+data03 = [1,2,3,4,5,4,2,6] # 마지막 결과형태인 set을 따라서 2,4,6이 한번씩만 출력이 됨
+s01 = (x for x in data03 if x % 2 == 0)
+print(s01)
+print(list(s01))
+print(list(s01)) # 두번째에는 나오지 않음

66ac42f4ee3a295e77c728dfa57094cf05d47120
김용선 <korea5781@gmail.com>
2020-06-15 15:21:17 +0900

    200615_07

--- aBasic/b_control_class/test.py
+++ aBasic/b_control_class/test.py
@@ -14,22 +14,52 @@
 # 4. {'students': 0, 'superuser': 1, 'professor': 2, 'employee': 3}
 # 6. Cat panda Owl -> ['Cat', 'Panda', 'Owl']
 # 7. Hong Gil University ->  DongUniversity (실수)
-# 8.
+# 8. zip
 # 9. [[12, 3], [15, 3], [12, 2]] -> [[12, 3], [15, 3]]
 # 10. tue, yellow -> yellow (2차원 배열 식으로 묶임, key value식으로 묶이는 게 아님)
+# 11. 72
+# 12. ??? ->
+#            ['a']
+#            ['a', '2']
+#            ['a', '2', 'error']
+# 13. ??? -> 80
+
+alist = ['a1', 'a2', 'a3']
+
+blist = ['b1', 'b2' ,'b3']
+
+for a, b in zip(alist, blist):
+    print(a, b)
+"""
+---------------테스트---------------
 
 
-week = ['mon', 'tue', 'wed', 'thu', 'fri', 'sat', 'sun']
-
-rainbow = ['red', 'orange', 'yellow', 'green', 'blue', 'navy', 'purple']
-
-list_data = [week, rainbow]
-
-print(list_data)
-
+---------------테스트---------------
 """
 
-"""
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 """
 ---------------문제---------------
@@ -44,5 +74,34 @@ b = [4, 5, ]
 c = [7, 8, 9]
 print([[sum(k), len(k)] for k in zip(a, b, c)])
 -> 부족한 부분은 아예 zip이 되지 않음
+
+12.
+alist = ["a", "b", "c"]
+blist = ["1", "2", "3"]
+abcd= []
+for a, b in enumerate(zip(alist, blist)):
+    try:
+        abcd.append(b[a])
+    except IndexError:
+        abcd.append("error")
+print(abcd)
+-> 아래 코드를 돌려보면 확인할 수 있음
+alist = ["a", "b", "c"]
+blist = ["1", "2", "3"]
+abcd= []
+for a, b in enumerate(zip(alist, blist)):
+    print(a)
+    print(b)
+    try:
+        abcd.append(b[a])
+    except IndexError:
+        abcd.append("error")
+print(abcd)
+13.
+alphabet = ["a", "b", "c", "d", "e", "f", "g", "h"]
+nums = [i for i in range(20)] # 0~19
+answer = [alpha+str(num) for alpha in alphabet for num in nums if num%2==0]
+print(len(answer))
+-> 80 (이중반복문으로 돌아가는듯)
 ---------------문제---------------
 """

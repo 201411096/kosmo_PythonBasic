aea12e8dc34f50b0e5a23fe2bd3f5df5b77ca0a0
김용선 <korea5781@gmail.com>
2020-06-29 10:18:44 +0900

    200629_02 urlparse

--- cWebConn/3_beautifulsoup_class/Ex07_alldown1.py
+++ cWebConn/3_beautifulsoup_class/Ex07_alldown1.py
@@ -7,7 +7,8 @@ from bs4 import BeautifulSoup
 from urllib import parse
 from urllib import request
 
-def enum_links(html,base):
+# 페이지에 연결되어 있는 링크들을 전부 절대경로의 형태로 return 해줌
+def enum_links(html, base):
     #-------------------------------------
     result = []
     soup = BeautifulSoup(html, 'html.parser')
@@ -16,14 +17,14 @@ def enum_links(html,base):
     # print('-'*50)                 
     for a in links:
         href = a.attrs['href']      # 상대경로와 절대경로가 섞여있음
-       #print(href)
+        # print(href)
         url = parse.urljoin(base, href)
         result.append(url)
         # index.html -> https://docs.python.org/3.7/library/index.html
         # ../start.html -> https://docs.python.org/3.7/start.html
         # http://py.com -> http://py.com
     return result
-
+        
 if __name__ == '__main__':
     url = 'https://docs.python.org/3.7/library/'
     response = request.urlopen(url)   # urllib.request.urlopen() : BeautifulSoup을 통해 html 파서할(데이타를 가져올) 대상
--- cWebConn/3_beautifulsoup_class/Ex07_alldown2.py
+++ cWebConn/3_beautifulsoup_class/Ex07_alldown2.py
@@ -9,8 +9,34 @@ from urllib import request
 import os, time, re  # re : 정규식
 
 def download_file(url):
-    pass
-
+    p = parse.urlparse(url)
+    print('1-', p)
+    savepath = "./"+p.netloc+p.path
+    print('2-', savepath)
+    # 마지막에 '/' 끝나면 index.html이 생략했다고 고려하여 index.html을 붙임
+    if re.search('/$', savepath):      # savepath가 '/'로 끝날경우
+        savepath += 'index.html'
+        print('3-', savepath)
+    
+    # 로컬 폴더에 파일이 존재하는지
+    if os.path.exists(savepath): return savepath
+    
+    # 다운받을 폴더가 없으면 생성
+    savedir = os.path.dirname(savepath)
+    if not os.path.exists(savedir):
+        os.makedirs(savedir)
+            # mkdir <-> makedirs
+            # mkdir : ..
+            # makedirs : 하위 폴더까지 생성을 함
+    # 파일 다운받기
+    try:
+        print('download : ', url)
+        request.urlretrieve(url, savepath)
+        time.sleep(2)
+        return savepath
+    except:
+        print('fail download ...', url)
+        return None
 
 if __name__ == '__main__':
     url = 'https://docs.python.org/3.6/library/'
@@ -18,4 +44,12 @@ if __name__ == '__main__':
     print(result)
 
 
-
+"""
+urlparse
+    ㄴ scheme 
+    ㄴ netloc 
+    ㄴ path 
+    ㄴ params 
+    ㄴ query 
+    ㄴ fragment 
+"""

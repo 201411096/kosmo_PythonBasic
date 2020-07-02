a5ce321ee1b3ac185eebca5afff0650c4decfdff
김용선 <korea5781@gmail.com>
2020-07-02 11:51:16 +0900

    200702_05

--- project/project_03/main3.py
+++ project/project_03/main3.py
@@ -2,11 +2,18 @@ import storedb
 import folium
 
 datalist = storedb.select_data()
-conversiondatalist = []
+addrList = []
 for idx, data in enumerate(datalist):
-    print(data)
+    #print(data)
     addr = storedb.conversion(data[1])
     if addr is None:
+        del datalist[idx]
         continue
-    conversiondatalist.append(data)
+    addrList.append(addr)
     print("main3.py ", idx, "addr 확인", addr)
+print("addrList확인", addrList)
+print("datalist 확인", datalist)
+map_osm = folium.Map(location=[addrList[0]['y'], addrList[0]['x']], zoom_start=5)
+for idx, conversiondata in enumerate(addrList):
+    folium.Marker(location=[addrList[idx]['y'], addrList[idx]['x']], popup=datalist[idx][1], icon=folium.Icon(color="red", icon="info-sign")).add_to(map_osm)
+map_osm.save("./map/result.html")

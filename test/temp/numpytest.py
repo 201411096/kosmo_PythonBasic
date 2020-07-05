1461a6654e60a226ca2f2685164c1a3118581984
201411096 <korea5781@gmail.com>
2020-07-06 00:06:47 +0900

    200706_01 numpy summary

---
+++ test/temp/numpytest.py
@@ -0,0 +1,11 @@
+import numpy as np
+
+origin = np.array([3,1,9,7])
+print('원본:', origin)
+
+
+sorted = origin.sort()
+print('원본:', origin)
+print('정렬 후 :', sorted)
+sorted2 = np.sort(origin)[::-1] # 모든 행, 모든 열에서 -1 을 추가로 표기하면 내림차순이 되버림
+print('내림차순', sorted2)

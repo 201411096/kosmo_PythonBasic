c50d0916a65218ebc14473954c4856c394afc9d9
김용선 <korea5781@gmail.com>
2020-07-06 17:36:33 +0900

    200706_10

--- test/temp/pandastest.py
+++ test/temp/pandastest.py
@@ -10,8 +10,13 @@ import re
 # print(str2[0])
 # print(str2[1])
 
-str2 = 'ab,c.de'
-str2 = re.split(', | . ', str2)
-print(str2)
-print(str2[0])
-print(str2[1])
+# str2 = 'ab,c.de'
+# str2 = re.split(', | . ', str2)
+# print(str2)
+# print(str2[0])
+# print(str2[1])
+
+list1 = []
+list2 = ['a']
+print(len(list1))
+print(len(list2))

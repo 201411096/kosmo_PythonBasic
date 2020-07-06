f219f4719e7df63cdb2300baaaf6fb87c15e2f25
김용선 <korea5781@gmail.com>
2020-07-06 16:05:16 +0900

    200706_08

--- test/temp/pandastest.py
+++ test/temp/pandastest.py
@@ -1,7 +1,17 @@
 
-str1 = 'abcde'
-print(str1.find('c'))
-print(str1[:str1.find('c')])
+# str1 = 'abcde'
+# print(str1.find('c'))
+# print(str1[:str1.find('c')])
 
+import re
+# str2 = 'ab,c.de'
+# str2 = str2.split(',|.')
+# print(str2)
+# print(str2[0])
+# print(str2[1])
 
-
+str2 = 'ab,c.de'
+str2 = re.split(', | . ', str2)
+print(str2)
+print(str2[0])
+print(str2[1])
